2c0d65db9ab5702a4683937d50ee6edc2e4dadea
danielthestranger <danielthestranger@gmail.com>
2018-06-14 12:00:43 +0200

    Renamed functions and extractedGetFieldValue()

--- Minesweeper.cs
+++ Minesweeper.cs
@@ -14,82 +14,88 @@ public static class Minesweeper
         public int LastRowIndex { get { return RowCount - 1; } }
         public int LastColIndex { get { return ColCount - 1; } }
 
-        public string[] MinesOnlyField { get; private set; }
-        public string[] AnnotatedField { get; private set; }
+        public string[] MinesOnlyBoard { get; private set; }
+        public string[] AnnotatedBoard { get; private set; }
 
         public Minefield(string[] input)
         {
-            MinesOnlyField = input;
-            ColCount = MinesOnlyField[0].Length;
-            RowCount = MinesOnlyField.Length;
+            MinesOnlyBoard = input;
+            ColCount = MinesOnlyBoard[0].Length;
+            RowCount = MinesOnlyBoard.Length;
 
-            AnnotateField();
+            AnnotateBoard();
         }
 
 
-        private void AnnotateField()
+        private void AnnotateBoard()
         {
-            char[,] annotatedField = new char[RowCount, ColCount];
+            char[,] annotatedBoard = new char[RowCount, ColCount];
 
-            int myCount = 0;
             for (int row = 0; row <= LastRowIndex; row++)
             {
                 for (int col = 0; col <= LastColIndex; col++)
                 {
-                    if (MinesOnlyField[row][col] == Minefield.Empty)
-                    {
-                        myCount = CountAdjacentMines(row, col);
-                        if (myCount == 0)
-                            annotatedField[row, col] = Minefield.Empty;
-                        else
-                            annotatedField[row, col] = Char.Parse(myCount.ToString());
-                    }
-                    else
-                    {
-                        annotatedField[row, col] = MinesOnlyField[row][col];
-                    }
+                    annotatedBoard[row, col] = GetFieldValue(row, col);
                 }
             }
 
-            AnnotatedField = new string[RowCount];
+            AnnotatedBoard = new string[RowCount];
 
             StringBuilder builder = new StringBuilder(ColCount);
             for (int row = 0; row <= LastRowIndex; row++)
             {
                 for (int col = 0; col <= LastColIndex; col++)
                 {
-                    builder.Append(annotatedField[row, col]);
+                    builder.Append(annotatedBoard[row, col]);
                 }
-                AnnotatedField[row] = builder.ToString();
+                AnnotatedBoard[row] = builder.ToString();
                 builder.Clear();
+            }
+
+            char GetFieldValue(int row, int col)
+            {
+                int myCount;
+                if (MinesOnlyBoard[row][col] == Minefield.Empty)
+                {
+                    myCount = CountAdjacentMines(row, col);
+                    if (myCount == 0)
+                        return Minefield.Empty;
+                    else
+                        return Char.Parse(myCount.ToString());
+                }
+                else
+                {
+                    return MinesOnlyBoard[row][col];
+                }
+            }
+
+            int CountAdjacentMines(int row, int col)
+            {
+                int totalCount = 0;
+
+                int peekedRow = 0;
+                int peekedCol = 0;
+
+                for (int dRow = -1; dRow <= 1; dRow++)
+                    for (int dCol = -1; dCol <= 1; dCol++)
+                    {
+                        peekedRow = row + dRow;
+                        peekedCol = col + dCol;
+                        if (peekedRow < 0
+                            || peekedRow > LastRowIndex
+                            || peekedCol < 0
+                            || peekedCol > LastColIndex
+                            || (dRow == 0 && dCol == 0)
+                            )
+                            continue;
+                        else if (MinesOnlyBoard[peekedRow][peekedCol] == Minefield.Mine)
+                            totalCount++;
+                    }
+
+                return totalCount;
             }
         }
 
-        private int CountAdjacentMines(int row, int col)
-        {
-            int totalCount = 0;
-
-            int peekedRow = 0;
-            int peekedCol = 0;
-
-            for (int dRow = -1; dRow <= 1; dRow++)
-                for (int dCol = -1; dCol <= 1; dCol++)
-                {
-                    peekedRow = row + dRow;
-                    peekedCol = col + dCol;
-                    if (peekedRow < 0
-                        || peekedRow > LastRowIndex
-                        || peekedCol < 0
-                        || peekedCol > LastColIndex
-                        || (dRow == 0 && dCol == 0)
-                        )
-                        continue;
-                    else if (MinesOnlyField[peekedRow][peekedCol] == Minefield.Mine)
-                        totalCount++;
-                }
-
-            return totalCount;
-        }
     }
 
     public static string[] Annotate(string[] input)
@@ -100,6 +106,6 @@ public static class Minesweeper
             return new string[0];
 
         Minefield field = new Minefield(input);
-        return field.AnnotatedField;
+        return field.AnnotatedBoard;
     }
 }
